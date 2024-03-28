3ffc7fe75b98dd217e59b970965d74bbdf80d1be
nicksadri <sadri.nick@gmail.com>
2024-03-28 13:50:04 -0400

    welcome and login pages (frontend)

--- app/src/main/java/com/example/spotifysdkimplementation/LoginPage.java
+++ app/src/main/java/com/example/spotifysdkimplementation/LoginPage.java
@@ -7,13 +7,12 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.spotifysdkimplementation.databinding.FragmentSecondBinding;
+import com.example.spotifysdkimplementation.databinding.LoginPageBinding;
 
-public class SecondFragment extends Fragment {
+public class LoginPage extends Fragment {
 
-    private FragmentSecondBinding binding;
+    private LoginPageBinding binding;
 
     @Override
     public View onCreateView(
@@ -21,7 +20,7 @@ public class SecondFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        binding = LoginPageBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
@@ -29,13 +28,13 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });
+//        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                NavHostFragment.findNavController(SecondFragment.this)
+//                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+//            }
+//        });
     }
 
     @Override
