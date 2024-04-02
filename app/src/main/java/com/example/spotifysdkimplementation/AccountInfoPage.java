5f5a905a6e5f73843f8bc96779d654256cbceee4
matthewhogan22 <matthewhogan2203@gmail.com>
2024-04-02 13:47:51 -0400

    Added Account Information page

---
+++ app/src/main/java/com/example/spotifysdkimplementation/AccountInfoPage.java
@@ -0,0 +1,49 @@
+package com.example.spotifysdkimplementation;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.spotifysdkimplementation.databinding.AccountCreationPageBinding;
+import com.example.spotifysdkimplementation.databinding.AccountInfoPageBinding;
+import com.example.spotifysdkimplementation.databinding.LoginPageBinding;
+import com.example.spotifysdkimplementation.databinding.LoginPageBinding;
+
+public class AccountInfoPage extends Fragment {
+    private AccountInfoPageBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = AccountInfoPageBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+//        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                NavHostFragment.findNavController(LoginPage.this)
+//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+//            }
+//        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
