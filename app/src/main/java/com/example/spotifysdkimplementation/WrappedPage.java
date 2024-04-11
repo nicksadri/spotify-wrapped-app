054b5137a2efb1633a65351fbda74fcfdd719817
CoderOfPrograms <ppremkumar3@gatech.edu>
2024-04-11 08:23:42 -0400

    Hopefully?

--- app/src/main/java/com/example/spotifysdkimplementation/WrappedPage.java
+++ app/src/main/java/com/example/spotifysdkimplementation/WrappedPage.java
@@ -6,6 +6,8 @@ import static com.example.spotifysdkimplementation.MainActivity.AUTH_TOKEN_REQUE
 import static com.example.spotifysdkimplementation.MainActivity.CLIENT_ID;
 import static com.example.spotifysdkimplementation.MainActivity.getRedirectUri;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,7 +20,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import com.example.spotifysdkimplementation.databinding.WelcomePageBinding;
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.spotifysdkimplementation.databinding.WrappedPageBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
@@ -27,6 +37,13 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.spotify.sdk.android.auth.AuthorizationClient;
 import com.spotify.sdk.android.auth.AuthorizationRequest;
 import com.spotify.sdk.android.auth.AuthorizationResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class WrappedPage extends AppCompatActivity {
 
@@ -50,6 +67,16 @@ public class WrappedPage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(WrappedPage.this, PreviousWrappedPage.class);
+                startActivity(intent);
+            }
+        });
+
+        Button chatButton = findViewById(R.id.chat_button);
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the Chat activity
+                Intent intent = new Intent(WrappedPage.this, chat.class);
                 startActivity(intent);
             }
         });
