a33649bbae4e567f375914db959bf4ebd9474f82
Advay Balakrishnan <advaybalak@gmail.com>
2024-04-21 16:51:24 -0400

    password and email updates

--- app/src/main/java/com/example/spotifysdkimplementation/ChangePasswordPage.java
+++ app/src/main/java/com/example/spotifysdkimplementation/ChangePasswordPage.java
@@ -1,5 +1,7 @@
 package com.example.spotifysdkimplementation;
 
+import static com.example.spotifysdkimplementation.MainActivity.currentUser;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +20,7 @@ import com.example.spotifysdkimplementation.databinding.ChangePasswordPageBindin
 import com.example.spotifysdkimplementation.databinding.LoginPageBinding;
 import com.example.spotifysdkimplementation.databinding.LoginPageBinding;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ChangePasswordPage extends AppCompatActivity {
@@ -38,12 +42,17 @@ public class ChangePasswordPage extends AppCompatActivity {
         oldPass = findViewById(R.id.old_password);
         newPass = findViewById(R.id.new_password);
         confPass = findViewById(R.id.confirm_password);
-
+        mAuth = FirebaseAuth.getInstance();
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // ADD LOGIC FOR CHANGING PASSWORD IN FIREBASE
-
+                if (!newPass.getText().toString().equals(confPass.getText().toString())) {
+                    Toast.makeText(ChangePasswordPage.this, "Password Update Failed.",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    mAuth.getCurrentUser().updatePassword(newPass.getText().toString());
+                }
                 Intent intent = new Intent(ChangePasswordPage.this, AccountInfoPage.class);
                 startActivity(intent);
             }
--- app/src/main/java/com/example/spotifysdkimplementation/ChangeUsernamePage.java
+++ app/src/main/java/com/example/spotifysdkimplementation/ChangeUsernamePage.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,11 +19,17 @@ import com.example.spotifysdkimplementation.databinding.AccountInfoPageBinding;
 import com.example.spotifysdkimplementation.databinding.ChangeUsernamePageBinding;
 import com.example.spotifysdkimplementation.databinding.LoginPageBinding;
 import com.example.spotifysdkimplementation.databinding.LoginPageBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ChangeUsernamePage extends AppCompatActivity {
     private ChangeUsernamePageBinding binding;
     private Button confirm, cancel;
     private EditText newUser, confUser;
+    private FirebaseAuth mAuth;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    static FirebaseUser currentUser;
 
 
     @Override
@@ -39,7 +46,12 @@ public class ChangeUsernamePage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // ADD LOGIC FOR CHANGING USERNAME IN FIREBASE
-
+                if (!newUser.getText().toString().equals(confUser.getText().toString())) {
+                    Toast.makeText(ChangeUsernamePage.this, "Username Update Failed.",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    mAuth.getCurrentUser().verifyBeforeUpdateEmail(newUser.getText().toString());
+                }
                 Intent intent = new Intent(ChangeUsernamePage.this, AccountInfoPage.class);
                 startActivity(intent);
             }
